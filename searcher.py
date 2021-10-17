8d6e046c3bc4793654a384fc8b0bcba79b2bdff9
roepfeli <85442544+roepfeli@users.noreply.github.com>
2021-10-17 19:10:51 +0200

    added basic top-level comment searching + usage of nextPageToken

--- searcher.py
+++ searcher.py
@@ -1,6 +1,12 @@
 import sys
 import requests
 import json
+import re
+
+# TODO: add more options like place of API-key, show relevant/time ordered comments, show only to-level comments, max number of comments etc...
+# TODO: add a simple mode: no need for a regex. Like grep default. But also make it possible to use a regex with an cli-option
+# TODO: make option to also show replies to comments
+
 
 baseRESTFULurl = "https://www.googleapis.com/youtube/v3"
 
@@ -17,10 +23,30 @@ if len(sys.argv) != 3:
 with open("./config.json") as f:
     config = json.load(f)
 
-comments = requests.get(baseRESTFULurl +
-                        "/commentThreads?part=snippet%2Creplies&videoId=" + sys.argv[1] + "&key=" + config["api_key"])
+nextPageToken = None
 
-# print(comments.text)
+counter = 0
 
-for comment in json.loads(comments.text)["items"]:
-    print(comment["snippet"]["topLevelComment"]["snippet"]["textDisplay"])
+while True:
+
+    if nextPageToken:
+        apiResult = requests.get(baseRESTFULurl +
+                                 "/commentThreads?part=snippet%2Creplies&pageToken=" + nextPageToken + "&maxResults=100&videoId=" + sys.argv[1] + "&key=" + config["api_key"])
+    else:
+        apiResult = requests.get(baseRESTFULurl +
+                                 "/commentThreads?part=snippet%2Creplies&maxResults=100&videoId=" + sys.argv[1] + "&key=" + config["api_key"])
+
+    apiResultJSON = json.loads(apiResult.text)
+
+    for commentObj in apiResultJSON["items"]:
+        counter += 1
+        comment = commentObj["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+        if re.match(sys.argv[2], comment):
+            print(comment)
+
+    try:
+        nextPageToken = apiResultJSON["nextPageToken"]
+    except KeyError:
+        break
+
+print(counter)
